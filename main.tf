4e09e06034bdfd6104f8f95c0a6aeb91a827c31a
adhi7862 <130149531+adhi7862@users.noreply.github.com>
2023-06-22 23:41:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,15 @@ resource "aws_instance" "my_ec2_instance" {
  instance_type = "t2.medium" 
  key_name = "projectpair" 
   
- tags ={ 
+  tags ={ 
  name="Terraform hands-on" 
 } 
-   user_data = <<-EOF
-    #!/bin/bash
-    apt-get update -y
-    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update -y
-    apt-get install -y docker-ce docker-ce-cli containerd.io
-    usermod -aG docker ubuntu
-  EOF
+ user_data  = <<-EOF
+ #!/bin/bash
+     sudo apt-get update -y
+     sudo apt install docker.io -y
+     sudo systemctl enable docker
+     sudo docker run -itd -p 8084:8081 adhi7862/docker-pipeline
+     sudo docker start $(docker ps -aq)
+EOF
 }
